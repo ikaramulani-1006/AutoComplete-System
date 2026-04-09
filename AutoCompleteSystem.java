814411b8991c3a308cb8b280383dfb21e40f255b
ikaramulani-1006 <ikaramulani2005@gmail.com>
2026-04-09 21:52:02 +0530

    Update AutoCompleteSystem.java

--- AutoCompleteSystem.java
+++ AutoCompleteSystem.java
@@ -83,3 +83,139 @@ class Trie {
         return shouldDelete && !node.isEnd && node.children.isEmpty();
     }
 }
+class AutoComplete {
+    Trie trie;
+
+    AutoComplete(Trie trie) { 
+        this.trie = trie; 
+    }
+
+    private void dfs(TrieNode node, char[] prefix, StringBuilder currentPrefix, String[][] results, int[] resultCount, int maxResults) {
+        if (node.isEnd && maxResults > 0) {
+            results[resultCount[0]] = new String[]{currentPrefix.toString(), String.valueOf(node.frequency)};
+            resultCount[0]++;
+        }
+        
+        // Converting HashMap keys to char array
+        Character[] keyArray = new Character[node.children.size()];
+        node.children.keySet().toArray(keyArray);
+        
+        // bubble sort for alphabetical order
+        for (int i = 0; i < keyArray.length - 1; i++) {
+            for (int j = 0; j < keyArray.length - i - 1; j++) {
+                if (keyArray[j] > keyArray[j + 1]) {
+                    Character temp = keyArray[j];
+                    keyArray[j] = keyArray[j + 1];
+                    keyArray[j + 1] = temp;
+                }
+            }
+        }
+        
+        for (int i = 0; i < keyArray.length; i++) {
+            char ch = keyArray[i];
+            if (resultCount[0] < maxResults) {
+                TrieNode child = node.children.get(ch);
+                currentPrefix.append(ch);
+                dfs(child, prefix, currentPrefix, results, resultCount, maxResults);
+                currentPrefix.deleteCharAt(currentPrefix.length() - 1);
+            }
+        }
+    }
+
+    String[] getSuggestions(String prefix, int maxResults, String sortBy) {
+        prefix = prefix.toLowerCase().trim();
+        TrieNode node = trie.root;
+        
+        // Finding prefix node 
+        for (int i = 0; i < prefix.length(); i++) {
+            char ch = prefix.charAt(i);
+            if (!node.children.containsKey(ch)) {
+                return new String[0]; // It is Empty array
+            }
+            node = node.children.get(ch);
+        }
+        
+        //array for results
+        String[][] results = new String[maxResults][2];
+        int[] resultCount = {0};
+        StringBuilder currentPrefix = new StringBuilder(prefix);
+        
+        dfs(node, prefix.toCharArray(), currentPrefix, results, resultCount, maxResults);
+        
+        // Converting to output array
+        String[] output = new String[resultCount[0]];
+        for (int i = 0; i < resultCount[0]; i++) {
+            output[i] = results[i][0];
+        }
+        
+        // sorting if frequency requested
+        if (sortBy.equals("frequency")) {
+            // Bubble sort by frequency (results[i][1])
+            for (int i = 0; i < output.length - 1; i++) {
+                for (int j = 0; j < output.length - i - 1; j++) {
+                    int freq1 = Integer.parseInt(results[j][1]);
+                    int freq2 = Integer.parseInt(results[j + 1][1]);
+                    if (freq1 < freq2) {
+                        String temp = output[j];
+                        output[j] = output[j + 1];
+                        output[j + 1] = temp;
+                    }
+                }
+            }
+        }
+        
+        return output;
+    }
+
+    void recordSearch(String word) {
+        word = word.toLowerCase().trim();
+        TrieNode node = trie.root;
+        for (int i = 0; i < word.length(); i++) {
+            char ch = word.charAt(i);
+            if (!node.children.containsKey(ch)) return;
+            node = node.children.get(ch);
+        }
+        if (node.isEnd) node.frequency++;
+    }
+
+    String[] getAllWords() {
+        String[][] results = new String[1000][2]; 
+        int[] resultCount = {0};
+        StringBuilder sb = new StringBuilder();
+        dfsAll(trie.root, sb, results, resultCount);
+        
+        String[] words = new String[resultCount[0]];
+        for (int i = 0; i < resultCount[0]; i++) {
+            words[i] = results[i][0];
+        }
+        return words;
+    }
+    
+    private void dfsAll(TrieNode node, StringBuilder prefix, 
+                       String[][] results, int[] resultCount) {
+        if (node.isEnd) {
+            results[resultCount[0]] = new String[]{prefix.toString(), String.valueOf(node.frequency)};
+            resultCount[0]++;
+        }
+        
+        Character[] keyArray = new Character[node.children.size()];
+        node.children.keySet().toArray(keyArray);
+        
+        // Bubble sort
+        for (int i = 0; i < keyArray.length - 1; i++) {
+            for (int j = 0; j < keyArray.length - i - 1; j++) {
+                if (keyArray[j] > keyArray[j + 1]) {
+                    Character temp = keyArray[j];
+                    keyArray[j] = keyArray[j + 1];
+                    keyArray[j + 1] = temp;
+                }
+            }
+        }
+        for (int i = 0; i < keyArray.length; i++) {
+            char ch = keyArray[i];
+            prefix.append(ch);
+            dfsAll(node.children.get(ch), prefix, results, resultCount);
+            prefix.deleteCharAt(prefix.length() - 1);
+        }
+    }
+}
